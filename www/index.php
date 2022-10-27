ba656b6f8594c7851e63a8e93f15d8ed9dd7c5c1
Kraugug <30536235+kraugug@users.noreply.github.com>
2022-10-27 03:55:20 +0100

    - Added testing radio list. Finally.

--- www/index.php
+++ www/index.php
@@ -45,7 +45,7 @@
         <h1><?php echo($xml->Name . " " . $xml->Version) ?></h1>
 	    <div style="margin-bottom: 20px;">Relesed <?php echo($xml->ReleaseDate) ?></div>
         <div style="margin-bottom: 10px;"><a href="?get">Download</a></div>
-        <div style="margin-bottom: 20px;"><a href="https://github.com/kraugug/TrayRadio/">GitHub</a></div>
+        <div style="margin-bottom: 10px;"><a href="https://github.com/kraugug/TrayRadio/">GitHub</a></div>
         <div style="margin-bottom: 20px;">Simple internet radio player for Windows inspired by Linux <a href="http://radiotray.sourceforge.net/">RadioTray</a>.</div>
         <div style="margin-bottom: 20px;">Using: <a href="http://www.un4seen.com/">BASS audio library</a></div>
         <div style="margin: 0 auto; margin-top: 20px; margin-bottom: 20px; display: inline-block;">
@@ -65,7 +65,11 @@
             </table>
         </div>
         <div style="margin-top: 20px; margin-bottom: 30px;">Changelog and download of the previous versions is in progress.</div>
-        <div class="cp">Copyright (c) 2017 Michal Heczko</div>
+        <div class="cp">Copyright (c)
+            <script>
+                var CurrentYear = new Date().getFullYear()
+                document.write(CurrentYear)
+            </script> Michal Heczko</div>
         <div class="cp">Michal Heczko &lt<a href="mailto:micky.heczko@gmail.com">micky.heczko@gmail.com</a>&gt</div>
     </body>
 </html>
